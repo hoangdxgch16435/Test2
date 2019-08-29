47a568cad5ccd6bdb2b847695a46dfb9f6b61422
hoangdxgch16435 <hoangdxgch16435@fpt.edu.vn>
2019-08-29 15:56:35 +0700

    1

--- index.php
+++ index.php
@@ -101,7 +101,7 @@
        
 
         <div class="container">
-            <center><img src="images/lv.jpg"></center>
+            <center><img src="Toy.png"></center>
             <div class="header">
                 
                 <div class="nav">
